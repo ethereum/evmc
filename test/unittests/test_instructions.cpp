6f1f961f85040a6e4a87f8810ea93fdfb3385495
Paweł Bylica <chfast@gmail.com>
2018-06-11 17:11:09 +0200

    Use name prefix for EVM opcode enum items

--- test/unittests/test_instructions.cpp
+++ test/unittests/test_instructions.cpp
@@ -11,27 +11,27 @@ TEST(instructions, tangerine_whistle_hard_fork)
     const auto h = evmc_get_instruction_metrics_table(EVMC_HOMESTEAD);
     const auto tw = evmc_get_instruction_metrics_table(EVMC_TANGERINE_WHISTLE);
 
-    EXPECT_EQ(h[EXTCODESIZE].gas_cost, 20);
-    EXPECT_EQ(tw[EXTCODESIZE].gas_cost, 700);
+    EXPECT_EQ(h[OP_EXTCODESIZE].gas_cost, 20);
+    EXPECT_EQ(tw[OP_EXTCODESIZE].gas_cost, 700);
 
-    EXPECT_EQ(h[EXTCODECOPY].gas_cost, 20);
-    EXPECT_EQ(tw[EXTCODECOPY].gas_cost, 700);
+    EXPECT_EQ(h[OP_EXTCODECOPY].gas_cost, 20);
+    EXPECT_EQ(tw[OP_EXTCODECOPY].gas_cost, 700);
 
-    EXPECT_EQ(h[BALANCE].gas_cost, 20);
-    EXPECT_EQ(tw[BALANCE].gas_cost, 400);
+    EXPECT_EQ(h[OP_BALANCE].gas_cost, 20);
+    EXPECT_EQ(tw[OP_BALANCE].gas_cost, 400);
 
-    EXPECT_EQ(h[SLOAD].gas_cost, 50);
-    EXPECT_EQ(tw[SLOAD].gas_cost, 200);
+    EXPECT_EQ(h[OP_SLOAD].gas_cost, 50);
+    EXPECT_EQ(tw[OP_SLOAD].gas_cost, 200);
 
-    EXPECT_EQ(h[CALL].gas_cost, 40);
-    EXPECT_EQ(tw[CALL].gas_cost, 700);
+    EXPECT_EQ(h[OP_CALL].gas_cost, 40);
+    EXPECT_EQ(tw[OP_CALL].gas_cost, 700);
 
-    EXPECT_EQ(h[CALLCODE].gas_cost, 40);
-    EXPECT_EQ(tw[CALLCODE].gas_cost, 700);
+    EXPECT_EQ(h[OP_CALLCODE].gas_cost, 40);
+    EXPECT_EQ(tw[OP_CALLCODE].gas_cost, 700);
 
-    EXPECT_EQ(h[DELEGATECALL].gas_cost, 40);
-    EXPECT_EQ(tw[DELEGATECALL].gas_cost, 700);
+    EXPECT_EQ(h[OP_DELEGATECALL].gas_cost, 40);
+    EXPECT_EQ(tw[OP_DELEGATECALL].gas_cost, 700);
 
-    EXPECT_EQ(h[SELFDESTRUCT].gas_cost, 0);
-    EXPECT_EQ(tw[SELFDESTRUCT].gas_cost, 5000);
+    EXPECT_EQ(h[OP_SELFDESTRUCT].gas_cost, 0);
+    EXPECT_EQ(tw[OP_SELFDESTRUCT].gas_cost, 5000);
 }
