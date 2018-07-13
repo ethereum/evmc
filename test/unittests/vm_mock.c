2e2b7f0e813fd460a397115334065b5460d209af
Paweł Bylica <chfast@gmail.com>
2018-07-13 15:31:49 +0200

    loader: Add unit tests for load_and_create()

--- test/unittests/vm_mock.c
+++ test/unittests/vm_mock.c
@@ -3,6 +3,7 @@
  * Licensed under the MIT License. See the LICENSE file.
  */
 
+#include <evmc/evmc.h>
 #include <evmc/utils.h>
 
 EVMC_EXPORT void* evmc_create_aaa()
@@ -13,4 +14,17 @@ EVMC_EXPORT void* evmc_create_aaa()
 EVMC_EXPORT void* evmc_create_eee_bbb()
 {
     return (void*)0xeeebbb;
+}
+
+EVMC_EXPORT void* evmc_create_failure()
+{
+    return NULL;
+}
+
+EVMC_EXPORT struct evmc_instance* evmc_create_abi42()
+{
+    static struct evmc_instance instance = {
+        .abi_version = 42,
+    };
+    return &instance;
 }
