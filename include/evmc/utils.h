445eadc98051ece7a48101ba7930f737dd92c198
Paweł Bylica <chfast@gmail.com>
2018-08-13 17:07:54 +0200

    docs: Document utils.h

--- include/evmc/utils.h
+++ include/evmc/utils.h
@@ -5,14 +5,29 @@
 
 #pragma once
 
+/**
+ * @addtogroup helpers
+ * @{
+ */
+
+/**
+ * @def EVMC_EXPORT
+ * Marks a function to be exported from a shared library.
+ */
 #if defined _MSC_VER || defined __MINGW32__
 #define EVMC_EXPORT __declspec(dllexport)
 #else
 #define EVMC_EXPORT __attribute__((visibility("default")))
 #endif
 
+/**
+ * @def EVMC_NOEXCEPT
+ * Safe way of marking a function with `noexcept` C++ specifier.
+ */
 #if __cplusplus
 #define EVMC_NOEXCEPT noexcept
 #else
 #define EVMC_NOEXCEPT
 #endif
+
+/** @} */
