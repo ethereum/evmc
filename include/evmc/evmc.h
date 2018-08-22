43fc619a7e940152e956d64797bdad04edcfb40b
Paweł Bylica <chfast@gmail.com>
2018-08-22 13:57:25 +0200

    Update license note in headers of source files

--- include/evmc/evmc.h
+++ include/evmc/evmc.h
@@ -3,7 +3,7 @@
  *
  * @copyright
  * Copyright 2018 The EVMC Authors.
- * Licensed under the MIT License. See the LICENSE file.
+ * Licensed under the Apache License, Version 2.0. See the LICENSE file.
  *
  * @defgroup EVMC EVMC
  * @{
