e614e2fbdd8fe5dec8001c3c9333ccd081c6f331
Alex Beregszaszi <alex@rtfs.hu>
2018-09-03 21:50:50 +0100

    Go binding: add EVMC_STORAGE_MODIFIED_DIRTY

--- bindings/go/evmc/host.go
+++ bindings/go/evmc/host.go
@@ -43,6 +43,7 @@ const (
 	StorageModified  StorageStatus = C.EVMC_STORAGE_MODIFIED
 	StorageAdded     StorageStatus = C.EVMC_STORAGE_ADDED
 	StorageDeleted   StorageStatus = C.EVMC_STORAGE_DELETED
+	StorageModifiedDirty StorageStatus = C.EVMC_STORAGE_MODIFIED_DIRTY
 )
 
 func goAddress(in C.struct_evmc_address) common.Address {
