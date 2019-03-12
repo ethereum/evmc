65d68faed21d6d9150a15fce83cc6c2ae85cdb27
Paweł Bylica <chfast@gmail.com>
2019-03-12 13:53:07 +0100

    go: Run unit tests with example_vm

--- bindings/go/evmc/evmc_test.go
+++ bindings/go/evmc/evmc_test.go
@@ -1,24 +1,26 @@
 // EVMC: Ethereum Client-VM Connector API.
-// Copyright 2018 The EVMC Authors.
-// Licensed under the Apache License, Version 2.0. See the LICENSE file.
+// Copyright 2019 The EVMC Authors.
+// Licensed under the Apache License, Version 2.0.
+
+//go:generate gcc -shared ../../../examples/example_vm.c -I../../../include -o example_vm.so
 
 package evmc
 
 import (
-	"os"
 	"testing"
 )
 
 func TestLoad(t *testing.T) {
-	i, err := Load(os.Getenv("EVMC_PATH"))
+	path := "./example_vm.so"
+	i, err := Load(path)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 	defer i.Destroy()
-	if i.Name() != "interpreter" {
-		t.Fatal("name is not 'interpreter'")
+	if i.Name() != "example_vm" {
+		t.Fatalf("name is %s", i.Name())
 	}
-	if i.Version()[0] != '1' {
-		t.Fatalf("version is %s", i.Version())
+	if i.Version()[0] < '0' || i.Version()[0] > '9' {
+		t.Fatalf("version number is weird: %s", i.Version())
 	}
 }
