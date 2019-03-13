58906e218be8fc90c95974d978f7c9f997b29c5a
Alex Beregszaszi <alex@rtfs.hu>
2019-03-13 13:41:04 +0100

    Add basic Rust bindings

---
+++ bindings/rust/build.rs
@@ -0,0 +1,25 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn gen_bindings() {
+    let bindings = bindgen::Builder::default()
+        .header("evmc.h")
+        // See https://github.com/rust-lang-nursery/rust-bindgen/issues/947
+        .trust_clang_mangling(false)
+        .generate_comments(true)
+        // https://github.com/rust-lang-nursery/rust-bindgen/issues/947#issuecomment-327100002
+        .layout_tests(false)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    gen_bindings();
+}
