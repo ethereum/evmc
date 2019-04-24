5112561d8521a6ded3ddd668972229df6b857fa5
Paweł Bylica <chfast@gmail.com>
2019-04-24 16:21:43 +0200

    rust: Add copyright notice headers to source files

--- bindings/rust/evmc-sys/src/lib.rs
+++ bindings/rust/evmc-sys/src/lib.rs
@@ -1,3 +1,8 @@
+/* EVMC: Ethereum Client-VM Connector API.
+ * Copyright 2019 The EVMC Authors.
+ * Licensed under the Apache License, Version 2.0.
+ */
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
