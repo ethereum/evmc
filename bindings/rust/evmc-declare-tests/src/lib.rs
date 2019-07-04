anandray/wvmc

rust: container.execute to return a Result

bindings/rust/evmc-declare-tests/src/lib.rs
@@ -19,7 +19,7 @@ impl EvmcVm for FooVM {
         FooVM { a: 105023 }
     }
 
-    fn execute(&self, code: &[u8], context: &ExecutionContext) -> ExecutionResult {
-        ExecutionResult::success(1337, None)
+    fn execute(&self, code: &[u8], context: &ExecutionContext) -> Result<ExecutionResult, ()> {
+        Ok(ExecutionResult::success(1337, None))
     }
 }

examples/example-rust-vm/src/lib.rs
@@ -14,13 +14,13 @@ impl EvmcVm for ExampleRustVM {
         ExampleRustVM {}
     }
 
-    fn execute(&self, code: &[u8], context: &ExecutionContext) -> ExecutionResult {
+    fn execute(&self, code: &[u8], context: &ExecutionContext) -> Result<ExecutionResult, ()> {
         let is_create = context.get_message().kind == evmc_sys::evmc_call_kind::EVMC_CREATE;
 
         if is_create {
-            ExecutionResult::failure()
+            Err(())
         } else {
-            ExecutionResult::success(66, Some(vec![0xc0, 0xff, 0xee]))
+            Ok(ExecutionResult::success(66, Some(vec![0xc0, 0xff, 0xee])))
         }
     }
 }

ethereum/evmc

bindings/rust/evmc-declare-tests/src/lib.rs
@@ -16,7 +16,7 @@ impl EvmcVm for FooVM {
         FooVM {}
     }
 
-    fn execute(&self, _code: &[u8], _context: &ExecutionContext) -> ExecutionResult {
-        ExecutionResult::success(1337, None)
+    fn execute(&self, _code: &[u8], _context: &ExecutionContext) -> Result<ExecutionResult, ()> {
+        Ok(ExecutionResult::success(1337, None))
     }
 }

examples/example-rust-vm/src/lib.rs
@@ -14,13 +14,13 @@ impl EvmcVm for ExampleRustVM {
         ExampleRustVM {}
     }
 
-    fn execute(&self, _code: &[u8], context: &ExecutionContext) -> ExecutionResult {
+    fn execute(&self, _code: &[u8], context: &ExecutionContext) -> Result<ExecutionResult, ()> {
         let is_create = context.get_message().kind() == evmc_sys::evmc_call_kind::EVMC_CREATE;
 
         if is_create {
-            ExecutionResult::failure()
+            Err(())
         } else {
-            ExecutionResult::success(66, Some(&[0xc0, 0xff, 0xee]))
+            Ok(ExecutionResult::success(66, Some(&[0xc0, 0xff, 0xee])))
         }
     }
 }