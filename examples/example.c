85498a1d424bde00dfa08365edd9e970fc92119d
Alex Beregszaszi <alex@rtfs.hu>
2019-08-08 12:51:41 +0200

    example: allow passing in evmc path as an argument

--- examples/example.c
+++ examples/example.c
@@ -14,17 +14,20 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char* argv[])
 {
 #ifdef STATICALLY_LINKED_EXAMPLE
+    (void)argc;
+    (void)argv;
     struct evmc_instance* vm = evmc_create_example_vm();
     if (!vm)
         return EVMC_LOADER_INSTANCE_CREATION_FAILURE;
     if (!evmc_is_abi_compatible(vm))
         return EVMC_LOADER_ABI_VERSION_MISMATCH;
 #else
+    const char* config_string = (argc > 1) ? argv[1] : "example-vm.so";
     enum evmc_loader_error_code error_code;
-    struct evmc_instance* vm = evmc_load_and_configure("example-vm.so", &error_code);
+    struct evmc_instance* vm = evmc_load_and_configure(config_string, &error_code);
     if (!vm)
     {
         printf("Loading error: %d\n", error_code);
