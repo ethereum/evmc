f39cc6debc167e0f0be2e7915f6822b4371d83b4
Paweł Bylica <chfast@gmail.com>
2020-01-03 13:57:18 +0100

    Add more storage statuses

--- include/evmc/evmc.h
+++ include/evmc/evmc.h
@@ -475,24 +475,27 @@ enum evmc_storage_status
     EVMC_STORAGE_UNCHANGED = 0,
 
     /**
-     * The value of a storage item has been modified: X -> Y.
-     */
-    EVMC_STORAGE_MODIFIED = 1,
-
-    /**
-     * A storage item has been modified after being modified before: X -> Y -> Z.
-     */
-    EVMC_STORAGE_MODIFIED_AGAIN = 2,
-
-    /**
      * A new storage item has been added: 0 -> X.
      */
-    EVMC_STORAGE_ADDED = 3,
+    EVMC_STORAGE_ADDED,
+
+    /**
+     * The value of a storage item has been modified: X -> Y.
+     */
+    EVMC_STORAGE_MODIFIED,
 
     /**
      * A storage item has been deleted: X -> 0.
      */
-    EVMC_STORAGE_DELETED = 4
+    EVMC_STORAGE_DELETED,
+
+    EVMC_DIRTY_ADDED_TO_DELETED,
+
+    EVMC_DIRTY_DELETED_TO_ADDED,
+    EVMC_DIRTY_MODIFIED_AGAIN,
+    EVMC_DIRTY_MODIFIED_TO_DELETED,
+    EVMC_DIRTY_MODIFIED_REVERTED,
+    EVMC_DIRTY_DELETED_REVERTED,
 };
 
 
