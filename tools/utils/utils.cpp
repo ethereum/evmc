856606ae442a504a5141807eab571f9189f60438
Paweł Bylica <chfast@gmail.com>
2020-09-30 10:05:50 +0200

    cmake: Extract evmc::tool-utils out of evmc::tool
    Move utils.{hpp,cpp} files to new evmc::tool-utils static library.
    It is going to be used in unit tests.

--- tools/utils/utils.cpp
+++ tools/utils/utils.cpp
@@ -1,8 +1,8 @@
-// evmone: Fast Ethereum Virtual Machine implementation
-// Copyright 2018-2019 The EVMC Authors.
+// EVMC: Ethereum Client-VM Connector API.
+// Copyright 2018-2020 The EVMC Authors.
 // Licensed under the Apache License, Version 2.0.
 
-#include "utils.hpp"
+#include <tools/utils/utils.hpp>
 #include <ostream>
 #include <stdexcept>
 
