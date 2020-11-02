815406e90c4d328a6d57cbfbe0def5451692e220
Paweł Bylica <chfast@gmail.com>
2020-11-02 20:41:20 +0100

    test: Add tool commands unit tests

---
+++ test/unittests/tool_commands_test.cpp
@@ -0,0 +1,48 @@
+// EVMC: Ethereum Client-VM Connector API.
+// Copyright 2020 The EVMC Authors.
+// Licensed under the Apache License, Version 2.0.
+
+#include "examples/example_vm/example_vm.h"
+#include "tools/commands/commands.hpp"
+#include <gtest/gtest.h>
+#include <sstream>
+
+using namespace evmc;
+
+namespace
+{
+std::string out_pattern(const char* rev,
+                        int gas_limit,
+                        const char* status,
+                        int gas_used,
+                        const char* output = nullptr)
+{
+    std::ostringstream s;
+    s << "Executing on " << rev << " with " << gas_limit << " gas limit\n\n"
+      << "Result:   " << status << "\nGas used: " << gas_used << "\n";
+    if (output)
+        s << "Output:   " << output << "\n";
+    return s.str();
+}
+}  // namespace
+
+TEST(tool_commands, run_empty_code)
+{
+    auto vm = evmc::VM{evmc_create_example_vm()};
+    std::ostringstream out;
+
+    const auto exit_code = cmd::run(vm, EVMC_FRONTIER, 1, "", out);
+    EXPECT_EQ(exit_code, 0);
+    EXPECT_EQ(out.str(), out_pattern("Frontier", 1, "failure", 1));
+}
+
+TEST(tool_commands, run_return_my_address)
+{
+    auto vm = evmc::VM{evmc_create_example_vm()};
+    std::ostringstream out;
+
+    const auto exit_code = cmd::run(vm, EVMC_HOMESTEAD, 200, "30600052596000f3", out);
+    EXPECT_EQ(exit_code, 0);
+    EXPECT_EQ(out.str(), out_pattern("Homestead", 200, "success", 200,
+                                     "0000000000000000000000000000000000000000"));
+}
