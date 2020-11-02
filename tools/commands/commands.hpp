331342cfa7996308933f89b66f15ef652c67144d
Paweł Bylica <chfast@gmail.com>
2020-11-02 20:18:05 +0100

    tools: Move "run" to separate evmc::tool-commands library

---
+++ tools/commands/commands.hpp
@@ -0,0 +1,19 @@
+// EVMC: Ethereum Client-VM Connector API.
+// Copyright 2020 The EVMC Authors.
+// Licensed under the Apache License, Version 2.0.
+
+#include <evmc/evmc.hpp>
+#include <iosfwd>
+#include <string>
+
+namespace evmc
+{
+namespace cmd
+{
+int run(evmc::VM& vm,
+        evmc_revision rev,
+        int64_t gas,
+        const std::string& code_hex,
+        std::ostream& out);
+}
+}  // namespace evmc
