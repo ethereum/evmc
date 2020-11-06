6c1787457f698dcbc16ad92aa649d26de16b67fd
Paweł Bylica <chfast@gmail.com>
2020-11-06 20:51:10 +0100

    tools: Move --vm to global options

--- tools/evmc/main.cpp
+++ tools/evmc/main.cpp
@@ -10,23 +10,40 @@ int main(int argc, const char** argv)
 {
     using namespace evmc;
 
-    CLI::App app{"EVMC tool"};
-    const auto& version_flag = *app.add_flag("--version", "Print version information");
-
     std::string vm_config;
     std::string code_hex;
     int64_t gas = 1000000;
     auto rev = EVMC_ISTANBUL;
 
+    CLI::App app{"EVMC tool"};
+    const auto& version_flag = *app.add_flag("--version", "Print version information");
+    const auto& vm_option =
+        *app.add_option("--vm", vm_config, "EVMC VM module")->envname("EVMC_VM");
+
     auto& run_cmd = *app.add_subcommand("run", "Execute EVM bytecode");
     run_cmd.add_option("code", code_hex, "Hex-encoded bytecode")->required();
-    run_cmd.add_option("--vm", vm_config, "EVMC VM module")->required()->envname("EVMC_VM");
     run_cmd.add_option("--gas", gas, "Execution gas limit", true)->check(CLI::Range(0, 1000000000));
     run_cmd.add_option("--rev", rev, "EVM revision", true);
 
     try
     {
         app.parse(argc, argv);
+
+        evmc::VM vm;
+        if (vm_option.count() != 0)
+        {
+            evmc_loader_error_code ec;
+            vm = VM{evmc_load_and_configure(vm_config.c_str(), &ec)};
+            if (ec != EVMC_LOADER_SUCCESS)
+            {
+                const auto error = evmc_last_error_msg();
+                if (error != nullptr)
+                    std::cerr << error << "\n";
+                else
+                    std::cerr << "Loading error " << ec << "\n";
+                return static_cast<int>(ec);
+            }
+        }
 
         // Handle the --version flag first and exit when present.
         if (version_flag)
@@ -37,17 +54,10 @@ int main(int argc, const char** argv)
 
         if (run_cmd)
         {
-            evmc_loader_error_code ec;
-            auto vm = VM{evmc_load_and_configure(vm_config.c_str(), &ec)};
-            if (ec != EVMC_LOADER_SUCCESS)
-            {
-                const auto error = evmc_last_error_msg();
-                if (error != nullptr)
-                    std::cerr << error << "\n";
-                else
-                    std::cerr << "Loading error " << ec << "\n";
-                return static_cast<int>(ec);
-            }
+            // For run command the --vm is required.
+            if (vm_option.count() == 0)
+                throw CLI::RequiredError{vm_option.get_name()};
+
             std::cout << "Config: " << vm_config << "\n";
             return cmd::run(vm, rev, gas, code_hex, std::cout);
         }
