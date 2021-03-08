bb2744fafd4222d36f650c72167d50b61a7483d0
Paweł Bylica <chfast@gmail.com>
2021-03-08 14:45:33 +0100

    test: Add hex utils unit tests

---
+++ test/unittests/utils_test.cpp
@@ -0,0 +1,49 @@
+// EVMC: Ethereum Client-VM Connector API.
+// Copyright 2021 The EVMC Authors.
+// Licensed under the Apache License, Version 2.0.
+
+#include "tools/utils/utils.hpp"
+#include <gtest/gtest.h>
+
+using namespace evmc;
+
+TEST(utils, hex_of_byte)
+{
+    EXPECT_EQ(hex(0x00), "00");
+    EXPECT_EQ(hex(0x01), "01");
+    EXPECT_EQ(hex(0x55), "55");
+    EXPECT_EQ(hex(0x7f), "7f");
+    EXPECT_EQ(hex(0x80), "80");
+    EXPECT_EQ(hex(0xff), "ff");
+}
+
+TEST(utils, hex_of_bytes)
+{
+    uint8_t data[] = {0x00, 0x01, 0x00, 0xa0, 0xff, 0x00, 0x55, 0x00};
+    EXPECT_EQ(hex(data, sizeof(data)), "000100a0ff005500");
+}
+
+TEST(utils, from_hex)
+{
+    EXPECT_EQ(from_hex(""), bytes{});
+    EXPECT_EQ(from_hex("00"), bytes{0x00});
+    EXPECT_EQ(from_hex("af"), bytes{0xaf});
+    EXPECT_EQ(from_hex("AF"), bytes{0xaf});
+    EXPECT_EQ(from_hex("Af"), bytes{0xaf});
+    EXPECT_EQ(from_hex("aF"), bytes{0xaf});
+    EXPECT_EQ(from_hex("00bc01000C"), (bytes{0x00, 0xbc, 0x01, 0x00, 0x0c}));
+}
+
+TEST(utils, from_hex_odd_length)
+{
+    EXPECT_THROW(from_hex("0"), std::length_error);
+    EXPECT_THROW(from_hex("1"), std::length_error);
+    EXPECT_THROW(from_hex("123"), std::length_error);
+}
+
+TEST(utils, from_hex_not_hex_digit)
+{
+    EXPECT_THROW(from_hex("0g"), std::out_of_range);
+    EXPECT_THROW(from_hex("000h"), std::out_of_range);
+    EXPECT_THROW(from_hex("ffffffzz"), std::out_of_range);
+}
