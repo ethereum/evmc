5ee7eef42972055bf5f114a3f9ae8d17605fe201
Paweł Bylica <chfast@gmail.com>
2021-03-08 14:10:55 +0100

    cmake: Upgrade CLI11 to 1.9.1

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -13,3 +13,11 @@ hunter_config(
     SHA1 152b849610d91a9dfa1401293f43230c2e0c33f8
     CMAKE_ARGS BUILD_GMOCK=OFF gtest_force_shared_crt=ON CMAKE_CXX_FLAGS=${gtest_cxx_flags}
 )
+
+hunter_config(
+    CLI11
+    VERSION 1.9.1
+    URL https://github.com/CLIUtils/CLI11/archive/v1.9.1.tar.gz
+    SHA1 8c32b19a5301726f1bf0b910a91a05d4291deadc
+    CMAKE_ARGS CLI11_BUILD_DOCS=OFF CLI11_BUILD_TESTS=OFF CLI11_BUILD_EXAMPLES=OFF CLI11_SINGLE_FILE=OFF
+)
