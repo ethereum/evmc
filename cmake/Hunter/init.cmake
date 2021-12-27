29d68d342e43495a64622053de60ad6d23fbcf3b
Paweł Bylica <chfast@gmail.com>
2021-12-27 10:56:49 +0100

    cmake: Upgrade Hunter and GTest

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -1,12 +1,12 @@
 # EVMC: Ethereum Client-VM Connector API.
-# Copyright 2018-2019 The EVMC Authors.
+# Copyright 2018 The EVMC Authors.
 # Licensed under the Apache License, Version 2.0.
 
 # Setup Hunter.
 # Hunter is going to be initialized only if building with tests,
 # where it is needed to get dependencies.
 HunterGate(
-    URL "https://github.com/ruslo/hunter/archive/v0.23.214.tar.gz"
-    SHA1 "e14bc153a7f16d6a5eeec845fb0283c8fad8c358"
+    URL "https://github.com/cpp-pm/hunter/archive/v0.23.320.tar.gz"
+    SHA1 "9b4e732afd22f40482c11ad6342f7d336634226f"
     LOCAL
 )
