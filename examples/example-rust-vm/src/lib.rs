eda05c6866ac06bd93d62b605cbec5839d85c221
Paweł Bylica <chfast@gmail.com>
2022-01-08 18:09:40 +0100

    EVMC 10.0.0-alpha.3
    Bump version: 10.0.0-alpha.2 → 10.0.0-alpha.3

--- examples/example-rust-vm/src/lib.rs
+++ examples/example-rust-vm/src/lib.rs
@@ -6,7 +6,7 @@
 use evmc_declare::evmc_declare_vm;
 use evmc_vm::*;
 
-#[evmc_declare_vm("ExampleRustVM", "evm, precompiles", "10.0.0-alpha.2")]
+#[evmc_declare_vm("ExampleRustVM", "evm, precompiles", "10.0.0-alpha.3")]
 pub struct ExampleRustVM;
 
 impl EvmcVm for ExampleRustVM {
