260c83ebfbc91a3898e599279c7823d519675cd9
Paweł Bylica <chfast@gmail.com>
2022-05-12 12:08:39 +0200

    Convert evmc::hex library into single hex.hpp header

--- include/evmc/hex.hpp
+++ include/evmc/hex.hpp
@@ -3,6 +3,7 @@
 // Licensed under the Apache License, Version 2.0.
 #pragma once
 
+#include <cctype>
 #include <cstdint>
 #include <iterator>
 #include <string>
@@ -26,9 +27,47 @@ enum class hex_errc
     /// Input contains incomplete hex byte (length is odd).
     incomplete_hex_byte_pair = 2,
 };
+}  // namespace evmc
+
+namespace std
+{
+/// Template specialization of std::is_error_code_enum for evmc::hex_errc.
+/// This enables implicit conversions from evmc::hex_errc to std::error_code.
+template <>
+struct is_error_code_enum<evmc::hex_errc> : true_type
+{};
+}  // namespace std
+
+namespace evmc
+{
 
 /// Obtains a reference to the static error category object for hex errors.
-const std::error_category& hex_category() noexcept;
+inline const std::error_category& hex_category() noexcept
+{
+    struct hex_category_impl : std::error_category
+    {
+        const char* name() const noexcept final { return "hex"; }
+
+        std::string message(int ev) const final
+        {
+            switch (static_cast<hex_errc>(ev))
+            {
+            case hex_errc::invalid_hex_digit:
+                return "invalid hex digit";
+            case hex_errc::incomplete_hex_byte_pair:
+                return "incomplete hex byte pair";
+            default:
+                return "unknown error";
+            }
+        }
+    };
+
+    // Create static category object. This involves mutex-protected dynamic initialization.
+    // Because of the C++ CWG defect 253, the {} syntax is used.
+    static const hex_category_impl category_instance{};
+
+    return category_instance;
+}
 
 /// Creates error_code object out of a hex error code value.
 inline std::error_code make_error_code(hex_errc errc) noexcept
@@ -49,23 +88,93 @@ inline std::string hex(uint8_t b) noexcept
     return {hex_chars[b >> 4], hex_chars[b & 0xf]};
 }
 
+/// Encodes bytes as hex string.
+inline std::string hex(bytes_view bs)
+{
+    std::string str;
+    str.reserve(bs.size() * 2);
+    for (const auto b : bs)
+        str += hex(b);
+    return str;
+}
+
+namespace internal_hex
+{
+inline constexpr int from_hex_digit(char h)
+{
+    if (h >= '0' && h <= '9')
+        return h - '0';
+    else if (h >= 'a' && h <= 'f')
+        return h - 'a' + 10;
+    else if (h >= 'A' && h <= 'F')
+        return h - 'A' + 10;
+    else
+        throw hex_error{hex_errc::invalid_hex_digit};
+}
+
+template <typename OutputIt>
+inline constexpr void from_hex(std::string_view hex, OutputIt result)
+{
+    // TODO: This can be implemented with hex_decode_iterator and std::copy.
+
+    // Omit the optional 0x prefix.
+    const auto hex_begin =
+        (hex.size() >= 2 && hex[0] == '0' && hex[1] == 'x') ? hex.begin() + 2 : hex.begin();
+
+    constexpr int empty_byte_mark = -1;
+    int b = empty_byte_mark;
+    for (auto it = hex_begin; it != hex.end(); ++it)
+    {
+        const auto h = *it;
+        if (std::isspace(h))
+            continue;
+
+        const int v = from_hex_digit(h);
+        if (b == empty_byte_mark)
+        {
+            b = v << 4;
+        }
+        else
+        {
+            *result++ = static_cast<uint8_t>(b | v);
+            b = empty_byte_mark;
+        }
+    }
+
+    if (b != empty_byte_mark)
+        throw hex_error{hex_errc::incomplete_hex_byte_pair};
+}
+}  // namespace internal_hex
+
 /// Validates hex encoded string.
-std::error_code validate_hex(std::string_view hex) noexcept;
+inline std::error_code validate_hex(std::string_view hex) noexcept
+{
+    struct noop_output_iterator
+    {
+        uint8_t sink = {};
+        uint8_t& operator*() noexcept { return sink; }
+        noop_output_iterator operator++(int) noexcept { return *this; }  // NOLINT(cert-dcl21-cpp)
+    };
+
+    try
+    {
+        internal_hex::from_hex(hex, noop_output_iterator{});
+        return {};
+    }
+    catch (const hex_error& e)
+    {
+        return e.code();
+    }
+}
 
 /// Decodes hex encoded string to bytes.
 ///
 /// Throws hex_error with the appropriate error code.
-bytes from_hex(std::string_view hex);
-
-/// Encodes bytes as hex string.
-std::string hex(bytes_view bs);
-}  // namespace evmc
-
-namespace std
+inline bytes from_hex(std::string_view hex)
 {
-/// Template specialization of std::is_error_code_enum for evmc::hex_errc.
-/// This enabled implicit conversions from evmc::hex_errc to std::error_code.
-template <>
-struct is_error_code_enum<evmc::hex_errc> : true_type
-{};
-}  // namespace std
+    bytes bs;
+    bs.reserve(hex.size() / 2);
+    internal_hex::from_hex(hex, std::back_inserter(bs));
+    return bs;
+}
+}  // namespace evmc
