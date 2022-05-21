34a5f672ea37a6b337c9c9411b6ece663dc5b396
Alex Beregszaszi <alex@rtfs.hu>
2022-05-21 15:58:00 +0200

    Drop const

--- bindings/rust/evmc-client/src/types.rs
+++ bindings/rust/evmc-client/src/types.rs
@@ -5,8 +5,6 @@
 
 pub use evmc_vm::{Capabilities, MessageKind, Revision, StatusCode, StorageStatus};
 
-pub const ADDRESS_LENGTH: usize = 20;
-pub const BYTES32_LENGTH: usize = 32;
-pub type Address = [u8; ADDRESS_LENGTH];
-pub type Bytes32 = [u8; BYTES32_LENGTH];
+pub type Address = [u8; 20];
+pub type Bytes32 = [u8; 32];
 pub type Bytes = [u8];
