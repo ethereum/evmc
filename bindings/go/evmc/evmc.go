d2df115d5b4a99da50e35cdb23f783143f3ae478
Andrei Maiboroda <andrei@ethereum.org>
2024-02-29 16:42:43 +0100

    Add EOFCREATE message support

--- bindings/go/evmc/evmc.go
+++ bindings/go/evmc/evmc.go
@@ -44,6 +44,8 @@ static struct evmc_result execute_wrapper(struct evmc_vm* vm,
 		*value,
 		{{0}}, // create2_salt: not required for execution
 		{{0}}, // code_address: not required for execution
+		0,     // code
+		0,     // code_size
 	};
 
 	struct evmc_host_context* context = (struct evmc_host_context*)context_index;
