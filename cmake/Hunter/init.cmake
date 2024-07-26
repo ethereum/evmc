782b34818677ce14ff1097c46778d9c3445a50d3
Paweł Bylica <pawel@ethereum.org>
2024-07-26 12:22:04 +0200

    cmake: Upgrade Hunter

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -6,6 +6,6 @@
 # Hunter is going to be initialized only if building with tests,
 # where it is needed to get dependencies.
 HunterGate(
-    URL "https://github.com/cpp-pm/hunter/archive/v0.25.3.tar.gz"
-    SHA1 "0dfbc2cb5c4cf7e83533733bdfd2125ff96680cb"
+    URL "https://github.com/cpp-pm/hunter/archive/v0.25.6.tar.gz"
+    SHA1 "69e4a05bd514bb05cb2c5aa02ce8ac420516aaf7"
 )
